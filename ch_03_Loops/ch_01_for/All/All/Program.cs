eedb9297cdeb0b7f724230bc304d7e4aa3594f0f
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-04-27 13:43:44 +0200

    add ch_03_Loops

---
+++ ch_03_Loops/ch_01_for/All/All/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+         
+            //we can not use the same varible i the same program
+            //int i = 10000000;
+            //but we can use it in anthor loop
+
+            /*########################################################################################################
+             * break .. going out from the loop
+             * ######################################################################################################*/
+            for (int i = 0; i <= 10; i++)
+            {
+                if(i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            /*###############################################################################################################
+             * continue .. going out from only this iteration 
+             * ##########################################################################################################*/ 
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
